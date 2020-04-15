1338ae8094b714e217eb3a03c5f6e1f291dcaaa1
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-15 19:49:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,6 +188,21 @@ def player_stats(player_name)
 end 
 
 
+# def player_stats(player_name)
+#   game_hash.each do |location, team|
+#     team.each do |attribute, data|
+#     if attribute == :players
+#       data.each do |player, stats|
+#         if player == player_name
+#           return stats
+#         end
+#       end
+#       end
+#     end
+#   end
+# end
+
+
 def player_stats(player_name)
   game_hash.each do |location, team|
     team.each do |attribute, data|
