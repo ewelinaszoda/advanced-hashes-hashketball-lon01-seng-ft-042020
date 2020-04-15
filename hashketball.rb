ea3c12cadc49eb1e412617ba29e67c7dff628193
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-15 19:05:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -137,8 +137,9 @@ def num_points_scored(player_name)
   game_hash.each do |location, data|
     data[:players].each do |player|
       if player[:player_name] == player_name
-        return player[:points] 
       end 
     end
   end
+  player[:points] 
 end
+
